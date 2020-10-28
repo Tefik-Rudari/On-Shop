1074a58324457e6bfafa9d446d257b223caa15af
Tefik Rudari <tefikrudarii@gmail.com>
2020-10-28 20:35:56 +0100

    fetching data from teh backend-no errors or warnings

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find(p => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [match])
 
   return (
     <>
@@ -38,7 +48,7 @@ const ProductScreen = ({ match }) => {
                   Price:
                 </Col>
                 <Col>
-                  <string>${product.price}</string></Col>
+                  <strong>${product.price}</strong></Col>
               </Row>
             </ListGroup.Item>
             <ListGroup.Item variant='flush'>
