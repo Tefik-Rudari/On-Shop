964dac2ef0b78e7e6883eda86604376f4553cce2
Tefik Rudari <tefikrudarii@gmail.com>
2020-11-24 00:07:22 +0100

    Product details reducer and action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 
 // This is an action - dispatch action to the reducers
@@ -25,6 +28,24 @@ export const listProducts = () => async (dispatch) => {
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
       // If it has a non generic(our made error) error than give us our error if not then the default
       // Also this gets the error from the backend shows it in the frontend (redux devtools)
+    })
+  }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     })
   }
 }
