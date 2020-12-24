d25346393ec0db81d6f15583cef49a6ef3b60ec0
Tefik Rudari <tefikrudarii@gmail.com>
2020-12-24 15:14:36 +0100

    User registration & password encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,9 +27,20 @@ const userSchema = mongoose.Schema(
   }
 )
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
+
+// This is middleware
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
